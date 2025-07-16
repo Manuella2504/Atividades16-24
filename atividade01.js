8abf242a175758691dbb364bcbaa8cf270a51052
Manuella <manuella.rosario@ba.estudante.senai.br>
2025-07-16 17:28:01 -0300

    Create atividade01.js

---
+++ atividade01.js
@@ -0,0 +1,17 @@
+let idade = 15;
+
+if(idade >= 0 && idade <= 6){
+    console.log("Infância");
+}
+else if(idade >= 7 && idade <= 9){
+    console.log("Segunda infância");
+}
+else if(idade >= 14 && idade <= 19){
+    console.log("Pré Adolescência");
+}
+else if(idade >= 20 && idade <= 29){
+    console.log("Juventude");
+}
+else if(idade >= 30 && idade <= 60){
+    console.log("Adulta");
+}
